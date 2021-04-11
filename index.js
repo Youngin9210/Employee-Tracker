070e7965c49cdebe16f4a3b53973f9625882cb01
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-11 17:17:02 +0000

    setting up connection for database

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+require("dotenv").config();
+const inquirer = require("inquirer");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.MySQL_HOST,
+
+  // Your port; if not 3306
+  port: process.env.MySQL_PORT,
+
+  // Your username
+  user: process.env.MySQL_USER,
+
+  // Be sure to update with your own MySQL password!
+  password: process.env.MySQL_PASS,
+  database: process.env.MySQL_DB,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected!!!");
+});
