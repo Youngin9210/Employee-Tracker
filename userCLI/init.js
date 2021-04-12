9ad968a02731f4a389a636d15524c25372f5a95b
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-12 19:56:37 +0000

    using a switch statement to determine what constructor class to run

--- userCLI/init.js
+++ userCLI/init.js
@@ -1,26 +1,36 @@
 const inquirer = require("inquirer");
+const UserViewQuery = require("./view.js");
+const view = new UserViewQuery();
 
 class initApp {
   init() {
-    inquirer.prompt([
-      {
-        type: "list",
-        name: "init",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Employees By Department",
-          "View All Employees By Manager",
-          "Add Employee",
-          "Remove Employee",
-          "Update Employee Role",
-          "Update Employee Manager",
-          "View All Roles",
-          "Add Role",
-          "Remove Role",
-        ],
-      },
-    ]);
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "init",
+          message: "What would you like to do?",
+          choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "View All Roles",
+            "Add Role",
+            "Remove Role",
+          ],
+        },
+      ])
+      .then((answer) => {
+        const { init } = answer;
+        switch (init) {
+          case "View All Employees By Department":
+            view.viewDepartment();
+        }
+      });
   }
 }
 
