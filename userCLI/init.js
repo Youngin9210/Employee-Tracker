8187575f7218076095bd08d0c039284f7e851c71
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-12 19:59:08 +0000

    added UserAddQuery requirements

--- userCLI/init.js
+++ userCLI/init.js
@@ -1,6 +1,8 @@
 const inquirer = require("inquirer");
 const UserViewQuery = require("./view.js");
 const view = new UserViewQuery();
+const UserAddQuery = require("./userCLI/add.js");
+const add = new UserAddQuery();
 
 class initApp {
   init() {
@@ -27,8 +29,12 @@ class initApp {
       .then((answer) => {
         const { init } = answer;
         switch (init) {
+          case "View All Employees":
+            view.viewEmployees();
+            break;
           case "View All Employees By Department":
             view.viewDepartment();
+            break;
         }
       });
   }
