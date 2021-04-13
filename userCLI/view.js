49fa02fd600d0a863c27f126a6a0ecf0f499c6d9
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-13 18:10:57 +0000

    adding some titles to table logs to look a little cleaner in the cmmand line

--- userCLI/view.js
+++ userCLI/view.js
@@ -29,12 +29,14 @@ class UserViewQuery {
         deptList.push(name);
         i++;
       });
+
       inquirer
         .prompt([
           {
             type: "list",
             name: "dept",
-            message: "What department would you like to view?",
+            message:
+              "What department would you like to view? (Reference above)",
             choices: deptList,
           },
         ])
@@ -45,6 +47,9 @@ class UserViewQuery {
           console.log(deptEmployees);
           let employees = [];
           connection.query(deptEmployees, (err, res) => {
+            console.log(
+              "\n================ EMPLOYEES BY DEPARTMENT ================"
+            );
             tLog(res);
           });
         });
@@ -54,6 +59,7 @@ class UserViewQuery {
   viewEmployees() {
     let employees = "SELECT * FROM employee";
     connection.query(employees, (err, res) => {
+      console.log("\n================ EMPLOYEES ================");
       tLog(res);
     });
   }
@@ -61,26 +67,30 @@ class UserViewQuery {
   viewRoles() {
     let roles = "SELECT * FROM role";
     connection.query(roles, (err, res) => {
+      console.log("\n================ Roles ================");
       tLog(res);
     });
   }
 
   viewManager() {
     let managers =
-      "SELECT id, first_name, last_name FROM employeeTracker_DB.employee WHERE manager_id IS NULL";
+      "SELECT e.id, e.first_name, e.last_name, r.title AS role, d.name AS dept FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id WHERE e.manager_id IS NULL";
     let managerList = [];
     connection.query(managers, (err, res) => {
       res.forEach(({ id }, i) => {
         managerList.push(id);
         i++;
       });
+
+      console.log("\n================ MANAGERS ================");
       tLog(res);
       inquirer
         .prompt([
           {
             type: "list",
             name: "manager",
-            message: "What is the ID of the manager you like to view?",
+            message:
+              "What is the ID of the manager you like to view? (Reference above)",
             choices: managerList,
           },
         ])
@@ -89,6 +99,9 @@ class UserViewQuery {
 
           let employees = `SELECT e.id, e.first_name, e.last_name, r.title AS role, r.salary, d.name AS department FROM employee e JOIN role r	ON e.role_id = r.id JOIN department d ON r.department_id = d.id WHERE e.manager_id = ${manager}`;
           connection.query(employees, (err, res) => {
+            console.log(
+              "\n================ EMPLOYEES BY MANAGER ================"
+            );
             tLog(res);
           });
         });
