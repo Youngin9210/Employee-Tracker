604b51410f022a84a771f378680a42389a1cfa65
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-14 18:23:30 +0000

    implimenting async/await and successfully updating employeeRole

--- userCLI/update.js
+++ userCLI/update.js
@@ -1,24 +1,51 @@
 const inquirer = require("inquirer");
+const UserViewQuery = require("./view.js");
 const LogTable = require("./assets/logTable.js");
 const tLog = new LogTable().log;
 
-require("dotenv").config();
+const connection = require("./assets/connection");
 
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: process.env.MySQL_HOST,
-
-  // Your port; if not 3306
-  port: process.env.MySQL_PORT,
-
-  // Your username
-  user: process.env.MySQL_USER,
-
-  // Be sure to update with your own MySQL password!
-  password: process.env.MySQL_PASS,
-  database: process.env.MySQL_DB,
-});
 class UserUpdateQuery {
-  updateRole() {}
+  async updateRole() {
+    const employees = "SELECT * FROM employee";
+    const roles = "SELECT * FROM role";
+    const employeeData = await connection.query(employees);
+    console.log("\n================ EMPLOYEES ================");
+    tLog(employeeData);
+
+    const eIDs = employeeData.map((row) => row.id);
+    const roleData = await connection.query(roles);
+    console.log("\n================ ROLES ================");
+    const rIDs = roleData.map((row) => row.id);
+    tLog(roleData);
+
+    const runPrompt = await inquirer.prompt([
+      {
+        type: "list",
+        name: "employeeID",
+        message:
+          "What is the id of the employee that you would like to update?",
+        choices: eIDs,
+      },
+      {
+        type: "list",
+        name: "roleID",
+        message: "What is the id of the employee's new role'?",
+        choices: rIDs,
+      },
+    ]);
+    const { employeeID, roleID } = runPrompt;
+    let updateRoleID = `UPDATE employee SET role_id = ${roleID} WHERE id = ${employeeID}`;
+
+    const update = await connection.query(updateRoleID, () => {
+      new UserViewQuery().viewEmployees();
+    });
+    console.log(update);
+
+    // connection.query(updateRoleID, (err, res) => {
+    //   new UserViewQuery().viewEmployees();
+    // });
+  }
 }
+
+module.exports = UserUpdateQuery;
