1ba6ae1225bb81f38b83d7f26f94ca12e7d294cf
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-14 17:31:49 +0000

    removed connection requirements to insert into connection.js to be more DRY

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-require("dotenv").config();
 const inquirer = require("inquirer");
-const mysql = require("mysql");
 var figlet = require("figlet");
 
 const initApp = require("./userCLI/init.js");
-
-const connection = mysql.createConnection({
-  host: process.env.MySQL_HOST,
-
-  // Your port; if not 3306
-  port: process.env.MySQL_PORT,
-
-  // Your username
-  user: process.env.MySQL_USER,
-
-  // Be sure to update with your own MySQL password!
-  password: process.env.MySQL_PASS,
-  database: process.env.MySQL_DB,
-});
+const app = new initApp();
 
 figlet("Employee", function (err, data) {
   if (err) {
@@ -36,8 +21,5 @@ figlet("Employee", function (err, data) {
     console.log(data);
   });
 });
-connection.connect((err) => {
-  if (err) throw err;
-  const app = new initApp();
-  app.init();
-});
+
+app.init();
