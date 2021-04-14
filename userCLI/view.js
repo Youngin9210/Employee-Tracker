2d75ea4fae2113c9c93014c7c90d36b81dbba7f2
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-14 20:01:35 +0000

    successfully viewing employees by department using async/await

--- userCLI/view.js
+++ userCLI/view.js
@@ -2,57 +2,31 @@ const inquirer = require("inquirer");
 const LogTable = require("./assets/logTable.js");
 const tLog = new LogTable().log;
 
-require("dotenv").config();
-
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: process.env.MySQL_HOST,
-
-  // Your port; if not 3306
-  port: process.env.MySQL_PORT,
-
-  // Your username
-  user: process.env.MySQL_USER,
-
-  // Be sure to update with your own MySQL password!
-  password: process.env.MySQL_PASS,
-  database: process.env.MySQL_DB,
-});
+const connection = require("./assets/connection");
 
 class UserViewQuery {
-  viewDepartment() {
-    let departments = "SELECT * FROM department";
-    let deptList = [];
-    connection.query(departments, (err, res) => {
-      res.forEach(({ name }, i) => {
-        deptList.push(name);
-        i++;
-      });
+  async viewDepartment() {
+    const departments = "SELECT * FROM department";
+    const deptData = await connection.query(departments);
+    const deptList = deptData.map((row) => row.id);
+    tLog(deptData);
 
-      inquirer
-        .prompt([
-          {
-            type: "list",
-            name: "dept",
-            message:
-              "What department would you like to view? (Reference above)",
-            choices: deptList,
-          },
-        ])
-        .then((answer) => {
-          const { dept } = answer;
-          // view employee by department
-          let deptEmployees = `SELECT e.id, e.first_name, e.last_name, r.title AS role, r.salary, d.name AS department FROM employee e JOIN role r	ON e.role_id = r.id JOIN department d	ON r.department_id = d.id	WHERE d.name = '${dept}'`;
-          let employees = [];
-          connection.query(deptEmployees, (err, res) => {
-            console.log(
-              "\n================ EMPLOYEES BY DEPARTMENT ================"
-            );
-            tLog(res);
-          });
-        });
-    });
+    const runPrompt = await inquirer.prompt([
+      {
+        type: "list",
+        name: "dept",
+        message: "What department would you like to view? (Reference above)",
+        choices: deptList,
+      },
+    ]);
+    const { dept } = runPrompt;
+    // view employee by department
+    let deptEmployees = `SELECT e.id, e.first_name, e.last_name, r.title AS role, r.salary, d.name AS department FROM employee e JOIN role r	ON e.role_id = r.id JOIN department d	ON r.department_id = d.id	WHERE d.id = ${dept}`;
+
+    const viewEmployees = await connection.query(deptEmployees);
+
+    console.log("\n================ EMPLOYEES BY DEPARTMENT ================");
+    tLog(viewEmployees);
   }
 
   viewEmployees() {
