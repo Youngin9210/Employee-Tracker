f68472607e7238faac9f0d81a9393b54465a895b
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-14 18:24:28 +0000

    having issues with figlet, so calling prompt method right away for now

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
 const inquirer = require("inquirer");
-var figlet = require("figlet");
 
 const initApp = require("./userCLI/init.js");
 const app = new initApp();
 
-figlet("Employee", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-
-  figlet("Manager", function (err, data) {
-    if (err) {
-      console.log("Something went wrong...");
-      console.dir(err);
-      return;
-    }
-    console.log(data);
-  });
-});
-
-app.init();
+app.prompt();
