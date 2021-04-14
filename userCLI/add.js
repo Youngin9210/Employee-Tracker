31b8f52d07331f8ee6c3995d0449072fa2d90c0e
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-14 19:45:53 +0000

    successfully adding new employee using async/await

--- userCLI/add.js
+++ userCLI/add.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
-const initApp = require("./init.js");
-const app = new initApp();
+
 const UserViewQuery = require("./view.js");
 const view = new UserViewQuery();
 const LogTable = require("./assets/logTable.js");
@@ -68,87 +67,68 @@ class UserAddQuery {
 
     const { role, role_salary, dept_id } = runPrompt;
     let newRole = `INSERT INTO role(title, salary, department_id) VALUES('${role}', '${role_salary}', '${dept_id}')`;
-    let selectRoles = "SELECT * FROM role";
 
-    const addRole = await connection.query(newRole, () => {
+    connection.query(newRole, () => {
       view.viewRoles();
     });
   }
 
-  addEmployee() {
-    let roles = "SELECT * FROM role";
-    let managers =
+  async addEmployee() {
+    const roles = "SELECT * FROM role";
+    const managers =
       "SELECT e.id, e.first_name, e.last_name, r.title AS role, d.name AS dept FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id WHERE e.manager_id IS NULL";
-    let roleList = [];
-    let managerList = ["NULL"];
-    const util = require("util");
-    const setTimeoutPromise = util.promisify(setTimeout);
-    connection.query(roles, (err, res) => {
-      res.forEach(({ id }, i) => {
-        roleList.push(id);
-        i++;
-      });
-      console.log("\n================ Roles ================");
-      tLog(res);
-    });
-    connection.query(managers, (err, res) => {
-      res.forEach(({ id }, i) => {
-        managerList.push(id);
-        i++;
-      });
-      console.log("\n================ Managers ================");
-      tLog(res);
-    });
 
-    setTimeoutPromise(15).then(() => {
-      inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "newFirst",
-            message: "What is the first name of the new employee?",
-            validate: (newFirst) => {
-              return newFirst
-                ? true
-                : console.log(
-                    "Please input the first name of the new employee."
-                  );
-            },
-          },
-          {
-            type: "input",
-            name: "newLast",
-            message: "What is the last name of the new employee?",
-            validate: (newLast) => {
-              return newLast
-                ? true
-                : console.log(
-                    "Please input the first name of the new employee."
-                  );
-            },
-          },
-          {
-            type: "list",
-            name: "roleID",
-            message: "What is the role id for the new employee?",
-            choices: roleList,
-          },
-          {
-            type: "list",
-            name: "managerID",
-            message:
-              "What is the new employee's manager's ID? If employee is a manager, select 'NULL'.",
-            choices: managerList,
-          },
-        ])
-        .then((answer) => {
-          const { newFirst, newLast, roleID, managerID } = answer;
-          let newEmployee = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('${newFirst}', '${newLast}', '${roleID}', ${managerID})`;
-          connection.query(newEmployee, (err, res) => {
-            if (err) throw err;
-            new UserViewQuery().viewEmployees();
-          });
-        });
+    console.log("\n================ Roles ================");
+    const roleData = await connection.query(roles);
+    const roleList = roleData.map((row) => row.id);
+    tLog(roleData);
+
+    console.log("\n================ MANAGERS ================");
+    const managerData = await connection.query(managers);
+    const managerList = managerData.map((row) => row.id);
+    managerList.push("NULL");
+    tLog(managerData);
+
+    const runPrompt = await inquirer.prompt([
+      {
+        type: "input",
+        name: "newFirst",
+        message: "What is the first name of the new employee?",
+        validate: (newFirst) => {
+          return newFirst
+            ? true
+            : console.log("Please input the first name of the new employee.");
+        },
+      },
+      {
+        type: "input",
+        name: "newLast",
+        message: "What is the last name of the new employee?",
+        validate: (newLast) => {
+          return newLast
+            ? true
+            : console.log("Please input the first name of the new employee.");
+        },
+      },
+      {
+        type: "list",
+        name: "roleID",
+        message: "What is the role id for the new employee?",
+        choices: roleList,
+      },
+      {
+        type: "list",
+        name: "managerID",
+        message:
+          "What is the new employee's manager's ID? If employee is a manager, select 'NULL'.",
+        choices: managerList,
+      },
+    ]);
+    const { newFirst, newLast, roleID, managerID } = runPrompt;
+    let newEmployee = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES('${newFirst}', '${newLast}', '${roleID}', ${managerID})`;
+
+    const addEmployee = await connection.query(newEmployee, () => {
+      view.viewEmployees();
     });
   }
 }
