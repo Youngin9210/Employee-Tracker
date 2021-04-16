0269042fd6275b59817339626bd306fbd73b27ea
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-16 17:04:17 +0000

    commenting code

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+// requiring initApp class from init.js
 const initApp = require("./userCLI/init.js");
+// setting app as a new initApp() instance
 const app = new initApp();
 
+// calling app.init() initialize app
 app.init();
