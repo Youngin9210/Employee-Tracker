af348e1fbb81f69f82bcde7b164f5a4cc5bfab55
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-16 02:14:01 +0000

    viewing employees a little more in depth

--- userCLI/view.js
+++ userCLI/view.js
@@ -13,7 +13,8 @@ class UserViewQuery {
   }
 
   async employees() {
-    const employees = "SELECT * FROM employee";
+    const employees =
+      "SELECT e.id, e.first_name, e.last_name, r.title, d.name as department, r.salary, CONCAT(manager.first_name, ' ', manager.last_name) as manager FROM employee e LEFT JOIN role r ON e.role_id = r.id LEFT JOIN department d ON r.department_id = d.id LEFT JOIN employee manager ON manager.id = e.manager_id";
     const employeeData = await connection.query(employees);
     console.log("\n================ EMPLOYEES ================\n");
     tLog(employeeData);
