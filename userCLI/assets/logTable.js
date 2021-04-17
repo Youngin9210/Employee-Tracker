2fad2bd378626c55dec01fc4367c29b40ea42507
Kyle Young <kyleyoung.9210@gmail.com>
2021-04-17 02:18:48 +0000

    commenting

--- userCLI/assets/logTable.js
+++ userCLI/assets/logTable.js
@@ -1,5 +1,7 @@
+// including console.table npm package
 const cTable = require("console.table");
 
+// constructor class to add a method to log table data using console.table
 class LogTable {
   log(x) {
     const table = cTable.getTable(x);
